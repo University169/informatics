b162e311475a5cb9a846f4f2065c2cb3e6316c5a
dmitriy.klimkov <dmitriy.klimkov@rt.ru>
2020-07-02 04:11:42 +0300

    Задание 27 тип 12 сократил и пофиксил

--- Ex27_type12.py
+++ Ex27_type12.py
@@ -2,21 +2,14 @@ n = int(input())
 d = 4
 nk = [0] * d
 kr = [0] * d
-for i in range(1, d):
+for i in range(1, d+1):
     x = int(input())
     if x % 3 == 0:
-        kr[i] += kr[i-1] + 1
-        nk[i] += nk[i-1]
+        kr[i % d] = kr[i-1] + 1
+        nk[i % d] = nk[i-1]
     else:
-        nk[i] += nk[i-1] + 1
-        kr[i] += kr[i-1]
-x = int(input())
-if x % 3 == 0:
-    kr[0] += kr[d] + 1
-    nk[0] += nk[d]
-else:
-    nk[0] += nk[d] + 1
-    kr[0] += kr[d]
+        nk[i % d] = nk[i-1] + 1
+        kr[i % d] = kr[i-1]
 # получили kr и nk - количество кратных и некратных к i%d моменту
 k = 0
 for i in range(d+1, n+1):
