08c2a03a24c69ea1208edce4921c213a2d22428b
dmitriy.klimkov <dmitriy.klimkov@rt.ru>
2020-07-02 04:18:45 +0300

    Задание 27 тип 12 окончательное оптимизированное

--- Ex27_type12.py
+++ Ex27_type12.py
@@ -1,25 +1,20 @@
+# Оптимальное решение
+
 n = int(input())
 d = 4
 nk = [0] * d
 kr = [0] * d
-for i in range(1, d+1):
-    x = int(input())
-    if x % 3 == 0:
-        kr[i % d] = kr[i-1] + 1
-        nk[i % d] = nk[i-1]
-    else:
-        nk[i % d] = nk[i-1] + 1
-        kr[i % d] = kr[i-1]
-# получили kr и nk - количество кратных и некратных к i%d моменту
 k = 0
-for i in range(d+1, n+1):
+for i in range(1, n+1):
     x = int(input())
     if x % 3 == 0:
-        k += nk[i % d]
+        if i >= (d+1):
+            k += nk[i % d]
         kr[i % d] = kr[(i-1) % d] + 1
         nk[i % d] = nk[(i-1) % d]
     else:
-        k += kr[i % d]
+        if i >= (d+1):
+            k += kr[i % d]
         nk[i % d] = nk[(i-1) % d] + 1
         kr[i % d] = kr[(i-1) % d]
 print(k)
